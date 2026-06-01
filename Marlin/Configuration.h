4af460de58d8eedc42f80b5f59557236fdfd011a
kevimax <104906965+kevimax@users.noreply.github.com>
2026-06-01 21:27:33 +0200

    Update Configuration.h
    Settings pour X1 + TFT V4.0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -91,14 +91,14 @@
 /*** Section 1 Artillery Printer ***/
 
 //#define GENIUS
-//#define X1
+#define X1
 //#define GENIUSPRO
 //#define SWD_X2
 //#define HORNET
 
 /*** Section 2 Board Type ***/
 
-//#define MKSGENL         // Stock Board for Genuis and X1
+#define MKSGENL         // Stock Board for Genuis and X1
 //#define RUBY            // Stock Board for Genuis Pro, X2 and Hornet
 //#define MKSGENLV21      // Choose this if you are using MKS GEN L V2.1
 //#define SKR13           // Choose this if you are using BigTreeTech SKR 1.3
@@ -123,14 +123,14 @@
 
 /*** Section 3 Extruder Type ***/
 
-//#define TITAN       // Stock Extruder
+#define TITAN       // Stock Extruder
 //#define BMG         // Choose this if you are using BMG/BMG Wind
 //#define HEMERA      // Choose this if you are using HEMERA
 //#define MATRIX      // Choose this if you are using MATRIX
 
 /*** Section 4 Drivers Type ***/
 
-//#define TMC_2100        // Stock Drivers
+#define TMC_2100        // Stock Drivers
 //#define TMC_2208_STA    // Standalone Mode
 //#define TMC_2209_STA    // Standalone Mode
 //#define LV_8729
@@ -139,7 +139,7 @@
 
 /*** Section 5 Bed Leveling ***/
 
-//#define BLTOUCH      //uncomment if you use a BLTouch
+#define BLTOUCH      //uncomment if you use a BLTouch
   #ifdef BLTOUCH
     #ifdef RUBY
     //#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
@@ -183,7 +183,7 @@
 
 //#define GraphicalLCD                              // Will work next to MKS TFT
 #define MKSGENL_TFT                               // To be activated if you have deported the TFT connection to EXP1 on the MKS Gen L ==> communication speed : 250000
-//#define FILAMENT_RUNOUT_SENSOR                    // If you connect your filament runout sensor to the motherboard instead of the TFT
+#define FILAMENT_RUNOUT_SENSOR                    // If you connect your filament runout sensor to the motherboard instead of the TFT
 //#define NEOPIXEL_PERSO                            // If you want to use a personal Neopixel LED on the Neopixel Port
 //#define LED_PORT_NEOPIXEL                         // If you want to use a personal Neopixel LED on the original LED Port
 //#define LINEAR_ADV                                // If you want to use Linear Advance
